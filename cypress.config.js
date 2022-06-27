e919bdb4cc103c4480df1a0452ec21961dc53b49
Thomas Erick <thomaserick.dev@gmail.com>
2022-06-26 21:59:55 -0300

    Cypress dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'pp55vb',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
