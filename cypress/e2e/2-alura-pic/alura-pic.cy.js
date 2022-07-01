63d0586780f5ae55e61d406de177270544b70834
Thomas Erick <thomaserick.dev@gmail.com>
2022-06-30 22:17:49 -0300

    Foreach

--- cypress/e2e/2-alura-pic/alura-pic.cy.js
+++ cypress/e2e/2-alura-pic/alura-pic.cy.js
@@ -57,6 +57,20 @@ describe("Login e registro de usuario alura pic", () => {
     cy.on('window:alert',(str) => {
       expect(str).to.equal('invalid user name or password')
     })
+
+    const usuarios = require('../../fixtures/usuarios.json');
+    usuarios.forEach(usuario => {
+
+        it(`registra novo usuario ${usuario.userName} `, () => {
+            cy.contains('a', 'Register now').click();
+            cy.contains('button', 'Register').click();
+            cy.get('input[formcontrolname="email"]').type(usuario.email);
+            cy.get('input[formcontrolname="fullName"]').type(usuario.fullName);
+            cy.get('input[formcontrolname="userName"]').type(usuario.userName);
+            cy.get('input[formcontrolname="password"]').type(usuario.password);
+            cy.contains('button', 'Register').click();
+        })
+    }); 
     
 
 
