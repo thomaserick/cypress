bb61fc71bf4fa036655f9080e70d3ddc4f36e069
Thomas Erick <thomaserick.dev@gmail.com>
2022-06-30 22:10:49 -0300

    Arquivos env cypress

--- cypress/e2e/2-alura-pic/api-alurapic.cy.js
+++ cypress/e2e/2-alura-pic/api-alurapic.cy.js
@@ -12,4 +12,18 @@ describe('Buscar fotos e dados ', () => {
             expect(res.body[0].description).to.be.equal('Farol iluminado')
         })
     })
+
+    it('fazer login do flavio',()=>{
+        cy.request({
+            method:'POST',
+            url:'https://apialurapic.herokuapp.com/user/login',
+            body: Cypress.env()
+        }).then((res)=>{
+            expect(res.status).to.be.equal(200)
+            expect(res.body).is.not.empty
+            expect(res.body).to.have.property('id')
+            expect(res.body.id).to.be.equal(1)
+          
+        })
+    })
 })
